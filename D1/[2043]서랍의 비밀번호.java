62d94fa50ad87d2abbceab3efc402569bd4eaeeb
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2024-10-31 03:42:17 +0900

    Create [2043]서랍의 비밀번호.java

---
+++ D1/[2043]서랍의 비밀번호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		/*
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			System.out.print("#");
+		}*/
+		
+		int p = sc.nextInt();
+		int k = sc.nextInt();
+		
+		System.out.println(p-k+1);
+		
+	}
+}
