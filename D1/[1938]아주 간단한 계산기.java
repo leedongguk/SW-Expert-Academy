db860ebf459e56193960ec9ba9c1e0f3b73c813f
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2024-10-31 03:50:19 +0900

    Create [1938]아주 간단한 계산기.java

---
+++ D1/[1938]아주 간단한 계산기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+/*
+		int T;
+		T=sc.nextInt();
+		int sum =0;
+		for(int test_case = 1; test_case <= T; test_case++)
+		{	
+			sum += test_case;
+		}
+		System.out.print(sum);
+
+		*/
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+	}
+}
+
+
+
