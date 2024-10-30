98975a2e9bcc78ba277e8c39abdb13a6851d87a7
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2024-10-31 04:04:25 +0900

    Create [1545]거꾸로 출력해 보아요.java

---
+++ D1/[1545]거꾸로 출력해 보아요.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+
+		int T;
+		T=sc.nextInt();
+		for(int test_case = T; test_case >= 0; test_case--)
+		{	
+			System.out.print(test_case+" ");
+		}
+		
+		
+	}
+}
+
+
+
