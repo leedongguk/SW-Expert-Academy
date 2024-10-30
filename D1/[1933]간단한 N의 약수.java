0eab0d142a68c7ec70ecd61bba1f479644954f1b
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2024-10-31 03:53:06 +0900

    Create [1933]간단한 N의 약수.java

---
+++ D1/[1933]간단한 N의 약수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{	
+			if(T%test_case == 0) {
+				System.out.print(test_case+" ");
+			}
+		}
+
+		
+
+		
+	}
+}
+
+
